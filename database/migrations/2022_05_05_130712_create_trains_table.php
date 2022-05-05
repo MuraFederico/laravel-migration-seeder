b926aea09ba0c02278e15ea0b6693ac63a701af9
MuraFederico <federicomura98@gmail.com>
2022-05-05 15:28:33 +0200

    created migration to create trains table

---
+++ database/migrations/2022_05_05_130712_create_trains_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->char('company', 100)->nullable();
+            $table->char('departure_station', 100);
+            $table->char('arrival_staion', 100);
+            $table->dateTime('departure_time', 0);
+            $table->dateTime('arrival_time', 0);
+            $table->smallInteger('train_code');
+            $table->tinyInteger('cart_number')->nullable();
+            $table->boolean('is_in_time')->default(0);
+            $table->boolean('is_cancelled')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
